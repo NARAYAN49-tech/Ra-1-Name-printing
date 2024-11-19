b338993155764022a76eb68042c141dd015d1308
Narayan Rajkumar Suryawanshi <narayanrsuryawanshi@gmail.com>
2024-11-19 21:20:01 +0530

    Add files via upload

---
+++ ravan.js
@@ -0,0 +1,13 @@
+// let h1 = document.querySelector("h1");
+// h1.textContent = "Ra - 1";
+// h1.style.color = "green";
+
+async function changeColor(color,rej) {
+         if(h1.style.color=="green"){
+            h1.style.color="red";
+        }
+         else{
+            h1.style.color="red";
+        }
+};
+changeColor(red,2000);
